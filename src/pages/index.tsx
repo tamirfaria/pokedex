8e2206ad079c215a7aa56f4e394409ef89d6017e
Lucas Tamir Faria <lucas.tamir@rarolabs.com.br>
2021-05-26 23:27:01 -0300

    feat: get api data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { PokemonsProps } from 'types/pokemons'
 
 const Home: React.FC = () => {
+  const [pokemons, setPokemons] = useState<PokemonsProps[]>([])
+  useEffect(() => {
+    axios.get('https://pokeapi.co/api/v2/pokedex/2/')
+      .then(response => {
+        if (response.data.pokemon_entries) {
+          setPokemons(response.data.pokemon_entries)
+        }
+      })
+      .catch(erro => console.log(erro))
+  }, [])
+
   return (
     <div>
-      Pokédex - Lucas Tamir
+      Pokédex | Lucas Tamir
+      <ul>
+        {pokemons.map((pokemon, index) => (
+          <div key={pokemon.entry_number}>
+            <li style={{ listStyle: 'none' }}>
+              <p>
+                {pokemon.entry_number} - <strong>{pokemon.pokemon_species.name}</strong>
+              </p>
+            </li>
+          </div>
+        ))}
+      </ul>
     </div>
   )
 }

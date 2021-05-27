0e2bd087a6e052030c704d9ddcb58d62f84aeb3d
Lucas Tamir Faria <lucas.tamir@rarolabs.com.br>
2021-05-26 23:29:27 -0300

    feat: get data api

--- src/types/pokemons.ts
+++ src/types/pokemons.ts
@@ -1,9 +1,7 @@
-export type Pokemons = {
-  pokemon_entries: {
-    entry_number: number
-    pokemon_species: {
-      name: string
-      url: string
-    }
+export type PokemonsProps = {
+  entry_number: number
+  pokemon_species: {
+    name: string
+    url: string
   }
 }

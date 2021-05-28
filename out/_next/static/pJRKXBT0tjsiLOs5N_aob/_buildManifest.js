de420f63871667ce636a4ddc507ecfa47afe34e7
Lucas Tamir Faria <lucas.tamir@rarolabs.com.br>
2021-05-28 19:30:40 -0300

    build: vercel build configuration

--- out/_next/static/pJRKXBT0tjsiLOs5N_aob/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-a40a5f94727096c62131.js"],"/_error":["static/chunks/pages/_error-d799e3a96772416696b7.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

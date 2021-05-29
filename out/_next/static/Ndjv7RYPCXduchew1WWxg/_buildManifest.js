f329169df3e57a7bfd131c24eed6d1d325f38a14
Lucas Tamir Faria <lucas.tamir@rarolabs.com.br>
2021-05-29 15:31:20 -0300

    build: test build

--- out/_next/static/Ndjv7RYPCXduchew1WWxg/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-6a52986659e20ec408ea.js"],"/_error":["static/chunks/pages/_error-d799e3a96772416696b7.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
